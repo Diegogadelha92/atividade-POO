7debd3c3c57489a505fdc25c7054211a1f7e9ae7
Diegogadelha92 <125842935+Diegogadelha92@users.noreply.github.com>
2023-02-26 17:48:45 -0300

    atividade lista 2 POO

---
+++ main/src/Main.java
@@ -0,0 +1,84 @@
+/*import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int  num1;
+        int  num2;
+        int  num3;
+        int  num4;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro numero :");
+        num1 = sc.nextInt();
+        System.out.print("Digite o segundo numero:");
+        num2 = sc.nextInt();
+        System.out.print("Digite o terceiro numero:");
+        num3 = sc.nextInt();
+        num4 = num1 + num2 + num3;
+        System.out.printf("O resultado da soma é :"+ num4);
+        sc.close();
+    }
+*/}
+
+
+/* public class Main {
+    public static void main(String[] Args) {
+        String data = mesPorExtenso(18, 2, 2023);
+
+        System.out.println(data);
+
+    }
+
+    public static String mesPorExtenso(int dia, int mes, int ano){
+        String nomeMes = "";
+
+        if (mes == 1){
+            nomeMes = "Janeiro";
+        }else if (mes == 2){
+            nomeMes = "Fevereiro";
+        }else if (mes == 3){
+            nomeMes = "Março";
+        }else if (mes == 4){
+            nomeMes = "Abril";
+        } else if (mes == 5){
+            nomeMes = "Maio";
+        } else if (mes == 6){
+            nomeMes = "Junho";
+        }else if(mes == 7){
+            nomeMes = "Julho";
+        }else if(mes == 8){
+            nomeMes = "Agosto";
+        }else if (mes == 9){
+            nomeMes = "Setembro";
+        }else if (mes == 10){
+            nomeMes = "Outubro";
+        }else if(mes == 11){
+            nomeMes = "Novembro";
+        }else if(mes == 12){
+            nomeMes = "Dezembro";
+        }
+        return String.format("%s de %d de %s" ,dia, nomeMes, ano);
+    }
+
+}
+*/
+
+/*public class Main {
+    public static void main(String[] Args) {
+
+    }
+    public class CalcImposto {
+
+        public static double somaImposto(double taxaImposto, double custo) {
+            double valorImposto = custo * (taxaImposto / 100.0);
+            double custoComImposto = custo + valorImposto;
+            return custoComImposto;
+        }
+        public static void main(String[] Args) {
+            double taxaImposto = 10.0;
+            double custo = 100.0;
+            double custoComImposto = somaImposto(taxaImposto, custo);
+            System.out.println("custo sem imposto: R$" + custo);
+            System.out.println("Custo com imposto: R$" + custoComImposto);
+        }
+    }
+}
+*/
